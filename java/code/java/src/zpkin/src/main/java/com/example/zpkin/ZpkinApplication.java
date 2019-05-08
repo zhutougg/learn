1d0d9e1ec421a34da954256349dca5d3e18e7eeb
taoxincheng <taoxincheng@163.com>
2019-05-08 17:07:26 +0800

    zipkin

--- java/code/java/src/zpkin/src/main/java/com/example/zpkin/ZpkinApplication.java
+++ java/code/java/src/zpkin/src/main/java/com/example/zpkin/ZpkinApplication.java
@@ -3,6 +3,7 @@ package com.example.zpkin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ZpkinApplication {
     private static final Logger LOG = Logger.getLogger(ZpkinApplication.class.getName());
     public static void main(String[] args) {
@@ -29,6 +31,7 @@ public class ZpkinApplication {
     public String index(){
         LOG.log(Level.INFO, "Index API is calling");
         ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://eurekaprovider/say","fdfdfd",String.class);
+        System.out.println(responseEntity.getBody());
         return "Welcome Sleuth!";
     }
 
--- java/code/java/src/zpkin/src/main/java/com/example/zpkin/init.java
+++ java/code/java/src/zpkin/src/main/java/com/example/zpkin/init.java
@@ -1,5 +1,6 @@
 package com.example.zpkin;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,5 +13,6 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class init {
     @Bean
+    @LoadBalanced
     RestTemplate restTemplate(){ return new RestTemplate(); }
 }
